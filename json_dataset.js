c3afcd6e225336e1838248c07d5c00f0ac2edb05
“[firstname <peza361@gmail.com>
2020-05-04 15:41:03 -0400

    small changes to scipt.js

--- json_dataset.js
+++ json_dataset.js
@@ -13,8 +13,13 @@ const quiz_data = {
 				"question": "What is the numerical range of a char data type in Java?",
 				"options": [ "-128 to 127", "0 to 256", "0 to 32767", "0 to 65535" ],
 				"answer": "0 to 65535",
+				"user_answer" : "1",
+				"correct" : false,
 				"explanation": " Char occupies 16-bit in memory, so it supports 2^16 i:e from 0 to 65535.",
-				"message": "Nice Job!"
+				"message": "Nice Job!",
+				"meta": {
+					"next_question": 2
+				}
 			},
 			{
 				"questionId": 2,
@@ -22,16 +27,51 @@ const quiz_data = {
 				"question": "Which of these coding types is used for data types of characters in Java?",
 				"options": ["ASCII", "ISO-LATIN-1", "UNICODE", "None of the above"],
 				"answer": "UNICODE",
+				"user_answer" : "1",
+				"correct" : false,
 				"explanation": "Unicode defines fully international character set that can represent all the characters found in all human languages. Its range is from 0 to 65536.",
-				"message":"Good Answer!"
+				"message":"Good Answer!",
+				"meta": {
+					"next_question": 3
+				}
 			}
 			],
-		},		
+		},
+		
 		{
 			"quizid": "2",
 			"title": "HTML and CSS Quiz",
-			"description": "Quiz with 21 questions on html and css code and structure"	
-		
+			"description": "Quiz with 21 questions on html and css code and structure",
+			data : [
+			{
+				"questionId": 1,
+				"questiontype": "MC",
+				"question": "What does HTML stand for?",
+				"options": [ "Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyper Text Model Layout" ],
+				"answer": "Hyper Text Markup Language",
+				"user_answer" : "1",
+				"correct" : false,
+				"explanation": "HTML stands for Hyper Text Markup Language",
+				"message": "Correct!",
+				"meta": {
+					"next_question": 2
+				}
+			},
+			{
+				"questionId": 2,
+				"questiontype": "MC",
+				"question": "Choose the correct HTML tag for the largest heading.",
+				"options": ["H1", "Heading", "Head", "H6"],
+				"answer": "H1",
+				"user_answer" : "1",
+				"correct" : false,
+				"explanation": "H1 is the tag for the largest heading, while H6 is the smallest heading.",
+				"message":"Good Answer!",
+				"meta": {
+					"next_question": 3
+				}
+			}			
+			]		
 		}
 		]  
 }
--- server.js.js
+++ server.js.js
@@ -2,46 +2,31 @@
 console.log("Hello, running script.js")
 
 // Require express library
-//require exword is like import a library
+// require exword is like import a library
 var express = require('express');
-
-//create an instance of express server.
+const PORT = 3000;
+const path = require('path');
 var app = express();
 
-//Require the module that defines the JSON object
+// Require the module that defines the JSON object
 const dataset = require('./json_dataset');
-//extract the specific JSON object that stores quiz data
 const quiz_data = dataset.quiz_data;
-//const question_data = quiz_data['list'][0]['data'];
-//console.log(question_data)
-//console.log(quiz_data)
-
-
-//---------------------------------------------------------------------------
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('json spaces', 2);
 // Start a server
-app.listen(3000, function() {
+app.listen(PORT, function() {
 	console.log('Example app listening on port 3000!');
 });
 
-// Define a route.
-app.get('/', function(req, res){
-	res.send('<h1>Hello World!</h1>');
+// HomePage -- TEST ROUTE -- Prints quiz HTML WEBSITE
+app.get('/', function(req, res){	
+	res.redirect('/api/quiz/list');
+	//res.render('')
 });
 
 
-
-// Define a TEST route.
-app.get('/api/quiz', (req, res) =>{
-	//res.send('<h1>Hello World!</h1>');
-	var quiz = {
-		q1: "What is 1+1",
-		a1: 2
-	}
-	res.json(quiz)
-});
-
-
-//--------------------Use-Case Routes--------------------------------------------------------------------------------
+//--------------------Use-Case Routes--------------------------------------------------------------------------------// 
 
 //returns json array with info about tests supported by api.
 //returns only the quiz information-> *quiz id, title, and description. NO quiz questions.*
@@ -52,7 +37,9 @@ app.get('/api/quiz/list', (req, res) =>{
 		delete quiz_info[i].data;
 	}		
 	
-	res.json(quiz_info)
+	res.sendFile(path.join(__dirname, 'Project4_IndividualProject_Quiz', 'index.html'));
+	res.json(quiz_info);
+	console.log("Inside the api/quiz/list")
 });
 
 //returns a single json object with the quiz information whose id equals to :quizid parameter in the route.
@@ -70,15 +57,100 @@ app.get('/api/quiz/:quizid', (req, res) =>{
 app.get('/api/quiz/:quizid/:questionid', (req, res) =>{
 	//get the url parameters 
 	var q_id = req.params['quizid'];	
-	var ques_id = req.params['questionid'];
+	var quest_id = req.params['questionid'];
 	
-	var question_data = quiz_data['list'][q_id]['data']
+	//var question_data = quiz_data['list'][q_id]['data']
 	
 	//var quiz_with_specific_id = quiz_data["list"].filter(q => (q.quizid == q_id) => (q.questionId == q.ques_id)) ;   
-	var specific_quiz_question = question_data.filter(qu => qu.questionId == ques_id); 
+	//var specific_quiz_question = question_data.filter(qu => qu.questionId == ques_id); 
 	//res.json(specific_quiz_question)
-	res.json(specific_quiz_question)
+	
+	// This should return an array of a single element (i.e. the quize with the id 1_id) 
+	var quiz_with_specific_id = quiz_data["list"].filter(q => q.quizid == q_id); 
+
+	// then you can filter out the question with id  quest_id, using somthing like. 
+	var question_with_specific_id = quiz_with_specific_id[0].data.filter(q => q.questionId == quest_id);
+	
+	//delete question_with_specific_id[0].questiontype;
+	//delete question_with_specific_id[0].question;
+	//delete question_with_specific_id[0].options;
+	delete question_with_specific_id[0].answer;
+	delete question_with_specific_id[0].user_answer;
+	delete question_with_specific_id[0].correct;
+	delete question_with_specific_id[0].explanation;
+	delete question_with_specific_id[0].message;
+	//delete question_with_specific_id[0].meta;
+
+	
+	res.json(question_with_specific_id)
 });
+
+//the route checks the answer parameter with the correct answer of the question with id 'questionID'
+//depending on true or false returns feedback view 
+app.get('/api/check_answer/:quizid/:questionid/:answer', (req, res) =>{
+	var q_id = req.params['quizid'];	
+	var quest_id = req.params['questionid'];
+	var answer = req.params['answer'];
+	
+	var quiz_with_specific_id = quiz_data["list"].filter(q => q.quizid == q_id); 
+	var question_with_specific_id = quiz_with_specific_id[0].data.filter(q => q.questionId == quest_id);
+	question_with_specific_id[0].user_answer = answer;  
+	
+	//console.log(question_with_specific_id[0].explanation)
+	console.log(answer)
+	console.log(question_with_specific_id[0].answer)
+	//if the answer is correct 
+	if (question_with_specific_id[0].user_answer == question_with_specific_id[0].answer){
+		question_with_specific_id[0].correct = true;
+		console.log(question_with_specific_id[0])
+		
+		delete question_with_specific_id[0].questiontype;
+		delete question_with_specific_id[0].question;
+		delete question_with_specific_id[0].options;
+		delete question_with_specific_id[0].answer;
+		delete question_with_specific_id[0].explanation;
+		delete question_with_specific_id[0].meta;
+		console.log(question_with_specific_id[0])
+		res.json(question_with_specific_id[0])
+		
+		//res.json(question_with_specific_id[0].questionId)
+		//res.json(question_with_specific_id[0].user_answer)
+		//res.json(question_with_specific_id[0].correct)
+		//res.json(question_with_specific_id[0].message)
+	}
+	
+	//if question is wrong 
+	else if (question_with_specific_id[0].user_answer != question_with_specific_id[0].answer){
+		question_with_specific_id[0].correct = false;
+		//console.log(question_with_specific_id[0].explanation)
+		
+		delete question_with_specific_id[0].questiontype;
+		delete question_with_specific_id[0].question;
+		delete question_with_specific_id[0].options;
+		delete question_with_specific_id[0].answer;
+		delete question_with_specific_id[0].message;
+		delete question_with_specific_id[0].meta;
+		console.log(question_with_specific_id[0])
+		res.json(question_with_specific_id[0])
+	}
+	
+	question_with_specific_id = quiz_with_specific_id[0].data.filter(q => q.questionId == quest_id);
+	//res.json(question_with_specific_id)
+});
+
+
+app.use((req,res)=> {
+	res.status(404)
+		.send('Unknown Request');
+});
+
+//delete quiz_info[i].data;
+
+
+
+
+
+
 
 
 
@@ -97,6 +169,26 @@ app.get('/api/quiz/:quizid/:questionid', (req, res) =>{
 	
 	//selected_question_id = quiz_with_specific_id.filter(q => q.questionId == )
 	//var selected_question_id = quiz_data["data"].filter(q =>q.questionId == ques_id);
+
+
+// Define a TEST route.
+app.get('/api/quiz', (req, res) =>{
+	//res.send('<h1>Hello World!</h1>');
+	var quiz = {
+		q1: "What is 1+1",
+		a1: 2
+	}
+	res.json(quiz)
+});
+
+
+
+
+
+
+
+
+
 
 */
 
